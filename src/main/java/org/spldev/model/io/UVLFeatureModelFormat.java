adc58ab0caf490ac035c5df5ceb607245bab9c6f
Elias Kuiter <info@elias-kuiter.de>
2022-07-11 13:39:03 +0200

    Updated license headers

--- src/main/java/org/spldev/model/io/UVLFeatureModelFormat.java
+++ src/main/java/org/spldev/model/io/UVLFeatureModelFormat.java
@@ -1,23 +1,23 @@
 /* -----------------------------------------------------------------------------
- * Formula Lib - Library to represent and edit propositional formulas.
- * Copyright (C) 2021-2022  Sebastian Krieter
- *
- * This file is part of Formula Lib.
- *
- * Formula Lib is free software: you can redistribute it and/or modify it
+ * uvl - Universal Variability Language
+ * Copyright (C) 2022 Elias Kuiter
+ * 
+ * This file is part of uvl.
+ * 
+ * uvl is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
- *
- * Formula Lib is distributed in the hope that it will be useful,
+ * 
+ * uvl is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Formula Lib.  If not, see <https://www.gnu.org/licenses/>.
- *
- * See <https://github.com/skrieter/formula> for further information.
+ * along with uvl. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * See <https://github.com/FeatJAR/uvl> for further information.
  * -----------------------------------------------------------------------------
  */
 package org.spldev.model.io;
--- src/test/java/org/spldev/model/io/UVLFeatureModelFormatTest.java
+++ src/test/java/org/spldev/model/io/UVLFeatureModelFormatTest.java
@@ -1,3 +1,25 @@
+/* -----------------------------------------------------------------------------
+ * uvl - Universal Variability Language
+ * Copyright (C) 2022 Elias Kuiter
+ * 
+ * This file is part of uvl.
+ * 
+ * uvl is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ * 
+ * uvl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with uvl. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * See <https://github.com/FeatJAR/uvl> for further information.
+ * -----------------------------------------------------------------------------
+ */
 package org.spldev.model.io;
 
 import org.junit.jupiter.api.Test;
