71ba8a14ab2e8266ecd0280682f8dd67dbdc7346
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2024-02-24 16:59:50 +0100

    Adapts test cases to use shared resources

--- src/test/java/de/featjar/feature/model/io/UVLFeatureModelFormatTest.java
+++ src/test/java/de/featjar/feature/model/io/UVLFeatureModelFormatTest.java
@@ -16,19 +16,16 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with uvl. If not, see <https://www.gnu.org/licenses/>.
  *
- * See <https://github.com/FeatJAR> for further information.
+ * See <https://github.com/FeatureIDE/FeatJAR-uvl> for further information.
  */
 package de.featjar.feature.model.io;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import de.featjar.Common;
 import de.featjar.base.FeatJAR;
-import de.featjar.base.computation.Cache;
 import de.featjar.base.data.Result;
 import de.featjar.base.io.IO;
-import de.featjar.base.log.CallerFormatter;
-import de.featjar.base.log.Log;
-import de.featjar.base.log.TimeStampFormatter;
 import de.featjar.feature.model.IFeatureModel;
 import de.featjar.feature.model.io.uvl.UVLFeatureModelFormat;
 import de.featjar.feature.model.transformer.ComputeFormula;
@@ -36,10 +33,10 @@ import de.featjar.formula.structure.Expressions;
 import de.featjar.formula.structure.formula.IFormula;
 import java.io.IOException;
 import java.nio.file.Paths;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class UVLFeatureModelFormatTest {
+public class UVLFeatureModelFormatTest extends Common {
+
     @Test
     public void graphVizFeatureModelFormat() throws IOException {
         Result<IFeatureModel> featureModel =
@@ -49,16 +46,5 @@ public class UVLFeatureModelFormatTest {
                 featureModel.toComputation().map(ComputeFormula::new).computeResult();
         assertTrue(formula.isPresent(), formula.printProblems());
         FeatJAR.log().info(Expressions.print(formula.get()));
-    }
-
-    @BeforeAll
-    public static void init() {
-        FeatJAR.configure()
-                .log(c -> c.logToSystemOut(Log.Verbosity.MESSAGE, Log.Verbosity.INFO, Log.Verbosity.PROGRESS))
-                .log(c -> c.logToSystemErr(Log.Verbosity.ERROR, Log.Verbosity.WARNING))
-                .log(c -> c.addFormatter(new TimeStampFormatter()))
-                .log(c -> c.addFormatter(new CallerFormatter()))
-                .cache(c -> c.setCachePolicy(Cache.CachePolicy.CACHE_NONE))
-                .initialize();
     }
 }
